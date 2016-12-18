dd32d0abbdc4068197959572211df3f7c654a492
Daniel Sánchez <danisanchez86@gmail.com>
2016-12-18 19:46:08 +0100

    Yellow Path - no funciona v1.0

--- app/services/sold-products-resolve.service.ts
+++ app/services/sold-products-resolve.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from "@angular/core";
+import { Resolve, ActivatedRouteSnapshot, Router} from "@angular/router";
+import { Observable } from "rxjs/Observable";
+
+import { Product } from "../models/product";
+import { ProductService } from "./product.service";
 
 @Injectable()
-export class SoldProductsResolve {
+export class SoldProductsResolve implements Resolve<any> {
 
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
     | Yellow Path                                                      |
@@ -19,4 +24,16 @@ export class SoldProductsResolve {
     |    Fíjate en qué se diferencia un producto a la venta de uno     |
     |    ya vendido; quizá te ayude con este punto.                    |
     |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+    constructor(private _productService: ProductService,
+                private _router: Router) { }
+
+
+    resolve(route: ActivatedRouteSnapshot): Observable<Product[]> {
+        return this._productService.getProducts();
+    }
+
+    botonReset(producto: Product): void {
+        this._router.navigate(["/reset", producto.state]);
+    }
 }
